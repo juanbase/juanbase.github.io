64bd4c8c35428208e811a01d288e31fd1f917ef8
Nguyen Viet Huan <ngnviethuan@gmail.com>
2021-06-01 16:28:01 +0700

    control access point

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,43 @@
 function noteAccessPoint({ ip, lat, long }) {
-  fetch("https://cfc58df0c569.ngrok.io/userAccessPoint", {
-    method: "POST",
+  return fetch('https://juanbase.herokuapp.com/userAccessPoint', {
+    method: 'POST',
     body: JSON.stringify({
       ip,
       lat,
       long,
     }),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-  })
-    .then((d) => d.json())
-    .then((d) => {
-      console.log(d);
-    });
+  }).then((d) => d.json());
 }
 
-fetch("https://api.ipify.org/?format=json")
-  .then((d) => d.json())
-  .then(({ ip }) => {
-    console.log(navigator.geolocation);
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        ({ coords: { latitude: lat, longitude: long } }) => {
-          noteAccessPoint({ ip, lat, long });
-        }
-      );
-    }
-    noteAccessPoint({ ip });
-  });
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + exdays);
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function readCookie(name) {
+  const nameEQ = name + '=';
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+const myPoint = readCookie('rePoint');
+
+if (!myPoint) {
+  fetch('https://api.ipify.org/?format=json')
+    .then((d) => d.json())
+    .then(({ ip }) => {
+      noteAccessPoint({ ip }).then(() => {
+        setCookie('rePoint', true, 15*60*1000);
+      });
+    });
+}
