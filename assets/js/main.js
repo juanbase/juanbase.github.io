e4d405d64ade82fae38fa400b59d7763205884c8
Nguyen Viet Huan <ngnviethuan@gmail.com>
2021-06-01 15:11:28 +0700

    control access point

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,31 @@
+function noteAccessPoint({ ip, lat, long }) {
+  fetch("https://cfc58df0c569.ngrok.io/userAccessPoint", {
+    method: "POST",
+    body: JSON.stringify({
+      ip,
+      lat,
+      long,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((d) => d.json())
+    .then((d) => {
+      console.log(d);
+    });
+}
+
 fetch("https://api.ipify.org/?format=json")
   .then((d) => d.json())
   .then(({ ip }) => {
-    fetch('https://juanbase.herokuapp.com/userAccessPoint', {
-      method: 'POST',
-      body: {
-        ip,
-      },
-    })
-      .then(d => d.json())
-      .then((d) => {
-        console.log(d);
-      })
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position);
+          noteAccessPoint({ ip, lat, long });
+        }
+      );
+    }
+    noteAccessPoint({ ip });
   });
