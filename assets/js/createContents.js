7d8cfb8dd4c555798e1479c2c85ab0b57c60a2d0
Nguyen Viet Huan <ngnviethuan@gmail.com>
2021-06-17 23:56:25 +0700

    release 1.0

--- assets/js/createContents.js
+++
@@ -1,7 +0,0 @@
-const createContents = (tag, contents) => {
-  const html = document.createElement(tag);
-  const txt = document.createElement('span');
-  txt.textContent = contents;
-  html.appendChild(txt);
-  return html;
-}
--- assets/js/vnd.js
+++
@@ -1,1 +0,0 @@
-const vnd = (num) => new Intl.NumberFormat('vn-VN', { style: 'currency', currency: 'VND' }).format(num);
